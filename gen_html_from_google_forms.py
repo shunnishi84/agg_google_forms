aaad2fe6114d2f1c904486b647fe8ee10912cfa6
Shunsuke Nishihara <shun@shun-spiral.tailc8ed9.ts.net>
2024-03-10 19:14:01 +0900

    flake8で指摘された部分を修正

--- gen_html_from_google_forms.py
+++ gen_html_from_google_forms.py
@@ -1,26 +1,30 @@
-import sys, re, math
+import sys
+import re
+import math
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import base64
-from collections import OrderedDict
 from io import BytesIO
 
-## 環境によってここのフォントを変える
+# 環境によってここのフォントを変える
 mpl.rcParams['font.family'] = 'Noto Sans JP'
+
 
 # グラフを描画してimgとして返す
 def plot_to_base64(labels, sizes):
-    plt.figure(figsize=(6,6))
-    
+    plt.figure(figsize=(6, 6))
+
     plt.pie(sizes, autopct='%1.1f%%', shadow=True, startangle=90)
-    plt.legend(loc="center left", bbox_to_anchor=(1, 0.5), labels=labels)  # 凡例を表示（かぶりを避ける）
+    plt.legend(loc="center left", bbox_to_anchor=(1, 0.5),
+               labels=labels)  # 凡例を表示（かぶりを避ける）
 
     png_image = BytesIO()
     plt.savefig(png_image, format='png', bbox_inches='tight', dpi=100)
     plt.clf()  # プロットエリアをクリア
     encoded = base64.b64encode(png_image.getvalue()).decode('utf-8')
-    f = f'<img src="data:image/png;base64,{encoded}">'
-    print(f)
+    img = f'<img src="data:image/png;base64,{encoded}">'
+    return img
+
 
 # html用スタイルシート
 def stylesheet():
@@ -33,7 +37,6 @@ def stylesheet():
         }
         li {
             padding-left: 8px;
-            
         }
         th {
             padding: 10px;
@@ -62,13 +65,14 @@ def stylesheet():
     </style>"""
     return css
 
+
 # 回答のdictをhtml化する
 def gen_html(question, data):
     # タグの先頭を出力
     html_base = f"""
 <!DOCTYPE html>
 <HTML><HEAD>{stylesheet()}</HEAD>"""
-    
+
     print(html_base)
     q_cnt = 1
 
@@ -84,23 +88,27 @@ def gen_html(question, data):
         # その他回答の配列
         other_ans = []
         # 円グラフプロット用のdict
-        plot_ans = {} 
+        plot_ans = {}
 
         title = f'<h2>Q{q_cnt}. {q}</h2>'
-        table_header ="<table><thead><tr><th>回答</th><th>回答数</th><th>回答割合</th></tr></thead>"
+        table_header = "<table><thead><tr><th>回答</th><th>回答数</th>\
+            <th>回答割合</th></tr></thead>"
         tables = ""
 
         # すべての回答が１種類しかないものは自由記述のものとして判断
-        if(max(ans.values())) < 3:
+        if (max(ans.values())) == 1:
             only_free_answer = True
 
         if not only_free_answer:
-            for a,cnt in ans.items():
+            for a, cnt in ans.items():
+                # 所属部署と勤続年数はその他があってもグラフに含む
                 if cnt > 1 or q in ['所属部署', '勤続年数']:
                     if a not in plot_ans:
                         # 円グラフプロット用の配列に追加
                         plot_ans[a] = cnt
-                    tables += f'<tr><td>{a}</td><td class=count>{cnt}</td><td class=count>{get_percentage(cnt, ans.values())}</td></tr>\n'
+                    tables += f'<tr><td>{a}</td><td class=count>{cnt}</td>\
+                        <td class=count>{get_percentage(cnt, ans.values())}\
+                        </td></tr>\n'
                 else:
                     other_ans.append(a)
                     # 回答数が1個しかないものは円グラフプロット用の配列に`その他`で追加
@@ -112,38 +120,42 @@ def gen_html(question, data):
         # html出力
         print(title)
         if not only_free_answer:
-            plot_to_base64(plot_ans.keys(), plot_ans.values())
+            print(plot_to_base64(plot_ans.keys(), plot_ans.values()))
 
             print(table_header)
             print(tables)
             print("</table>")
 
-            if len(other_ans)!=0:
+            if len(other_ans) != 0:
                 print("<h3>その他には以下のような回答が寄せられています</h3>")
                 for a in other_ans:
-                    print(parse_text(a))
+                    print(format_text(a))
         else:
             for key, value in ans.items():
-                print(parse_text(key))
+                print(format_text(key))
 
         q_cnt += 1
 
 
+# 回答率
 def get_percentage(cnt, nums):
     percentage = f'{math.floor((cnt / sum(nums))* 10000)/100}%'
     return percentage
 
-def parse_text(intext):
+
+# フリーアンサー内で `・`とかmarkdownっぽい表記しているのを見やすくする
+def format_text(intext):
     response = ""
     for a in intext.split(" ・"):
         a = re.sub(r'^・', '', a)
-        a = re.sub(r'\- ','<br>',re.sub(r'^\-','',a))
-        a = re.sub(r'## ','<li>', a)
+        a = re.sub(r'\- ', '<br>', re.sub(r'^\-', '', a))
+        a = re.sub(r'## ', '<li>', a)
         a = re.sub(r'\<li\>\<li\>', '', a)
         if a != '':
-            response += re.sub(r'<li><li>','<li>',f'<li>{a}</li>\n')
+            response += re.sub(r'<li><li>', '<li>', f'<li>{a}</li>\n')
 
     return response
+
 
 # メイン処理
 if len(sys.argv) == 1:
@@ -155,12 +167,10 @@ cont = open(sys.argv[1], 'r').read().split("\n")
 # ヘッダ行は質問として取得
 question = cont[0].split("\t")
 
-# それ以降を回答と取得
+# それ以降を回答として取得
 cont = cont[1:]
+ans = {}
 
-ans = OrderedDict()
-
-# print(type(ans))
 for line in cont:
     for i, val in enumerate(line.split("\t")):
         # 設問とわず回答が空であるものはスキップ
@@ -171,11 +181,11 @@ for line in cont:
         for v in val.split(", "):
             # 設問そのものが配列にない場合
             if question[i] not in ans:
-                ans[question[i]] = {v : 1}
+                ans[question[i]] = {v: 1}
             else:
                 # 設問はあるけど同様の回答がない場合
                 if v not in ans[question[i]]:
-                    ans[question[i]].update({v :1})
+                    ans[question[i]].update({v: 1})
                 else:
                     ans[question[i]][v] += 1
 
